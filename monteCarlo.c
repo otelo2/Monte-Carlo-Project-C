9887a482d0dbd9482887fb0a2f118fcad99b57f9
Otelo2 <josean2009@live.com.mx>
2020-04-02 16:10:37 -0600

    add values for square, sphere and random points

--- monteCarlo.c
+++ monteCarlo.c
@@ -4,11 +4,26 @@
 
 int main(int argc, char *argv[])
 {
+    srand (time(NULL)); //set seed for random number generator
     int nthreads, tid;
+    int pointsAmount;
+
+    //Create the base square (2r)
+    int squareSide = 10;
+    int squareArea = squareSide*squareSide;
+
+    //Create the circle (r)
+    //area for a circle = pi*(r*r)
+    float sphereRadius = 10/2; //This is r
+    float sphereArea = 3.14159 * (sphereRadius*sphereRadius);
 
     //Fork a team of threads giving then their own copies of variables
     #pragma omp parallel private(nthreads, tid)
     {
+        //Create a random amount of points
+        //where N is the max num of points
+        int N=500;
+        pointsAmount = rand() % (N+1); 
 
         //obtain thread number
         tid= omp_get_thread_num();
@@ -22,4 +37,6 @@ int main(int argc, char *argv[])
         }
 
     } //All threads join master thread and disband
+
+    return 0;
 }
