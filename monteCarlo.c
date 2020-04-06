d41bb0690979095d80a8290e8ae503318203076c
Otelo2 <josean2009@live.com.mx>
2020-04-05 20:14:30 -0500

    Create points function and better variables

--- monteCarlo.c
+++ monteCarlo.c
@@ -2,21 +2,42 @@
 #include <stdlib.h>
 #include <omp.h>
 
+//TODO: See if we really need the areas, i dont think so.
+
+//Declare global variables for our shapes properties
+int squareSide, squareArea;
+float sphereRadius, sphereArea;
+
+void create_points()
+{
+    srand (time(NULL)); //set seed for random number generator
+    int pointsAmount, interval;
+    int angle,position; //Properties of the point
+    int insideCircle=0,outsideCircle=0; //Counter of points inside and out the circle.
+    interval = 500;
+    pointsAmount = rand() % (interval+1);
+    for(int i=0; i<pointsAmount; i++)
+    {
+        //this will only work if a random number is generated each cycle, lets pray.
+
+    }
+}
+
 int main(int argc, char *argv[])
 {
     srand (time(NULL)); //set seed for random number generator
     int nthreads, tid;
     int pointsAmount;
 
-    //Create the base square (2r)
-    int squareSide = 10;
-    int squareArea = squareSide*squareSide;
+    //Give values to the base square (2r)
+    squareSide = 10;
+    squareArea = squareSide*squareSide;
     printf("Square:\n Side: %d. Area: %d\n",squareSide,squareArea);
 
-    //Create the circle (r)
+    //Give values to the circle (r)
     //area for a circle = pi*(r*r)
-    float sphereRadius = 10/2; //This is r
-    float sphereArea = 3.14159 * (sphereRadius*sphereRadius);
+    sphereRadius = squareSide/2; //This is r
+    sphereArea = 3.14159 * (sphereRadius*sphereRadius);
     printf("Sphere:\n Radius: %f. Area %0.4f\n",sphereRadius,sphereArea);
 
     //Fork a team of threads giving then their own copies of variables
